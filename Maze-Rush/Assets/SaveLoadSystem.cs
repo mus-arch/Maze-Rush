f5ca0e6a9b62029935d1f4c7db4a71e0d95ef85a
AhmadAlwakked <alwakedahmad6@gmail.com>
2025-04-04 20:51:18 +0200

    re

--- Maze-Rush/Assets/SaveLoadSystem.cs
+++ Maze-Rush/Assets/SaveLoadSystem.cs
@@ -1,45 +1,44 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; // Zorg dat dit erbij staat
 
 public class SaveLoadSystem : MonoBehaviour
 {
-    public Transform player; // Speler-object
-
-    public void Save()
-    {
-        // Spelerpositie opslaan
-        PlayerPrefs.SetFloat("X", player.position.x);
-        PlayerPrefs.SetFloat("Y", player.position.y);
-        PlayerPrefs.SetFloat("Z", player.position.z);
-        PlayerPrefs.Save();
-
-        // Ga naar het hoofdmenu
-        SceneManager.LoadScene("Main Menu");
-    }
-
-    public void Continue()
-    {
-        // Ga naar de game-scene
-        SceneManager.LoadScene("GAME");
-    }
-
-    public void NewGame()
-    {
-        // Wis alle opgeslagen data en start opnieuw
-        PlayerPrefs.DeleteAll();
-        SceneManager.LoadScene("GAME");
-    }
+    public Button saveButton;
+    public Button continueButton;
+    public Button newGameButton;
 
     void Start()
     {
-        // Alleen laden als er een opgeslagen positie is
-        if (PlayerPrefs.HasKey("X"))
-        {
-            player.position = new Vector3(
-                PlayerPrefs.GetFloat("X"),
-                PlayerPrefs.GetFloat("Y"),
-                PlayerPrefs.GetFloat("Z")
-            );
-        }
+        transform.position = new Vector3(
+            PlayerPrefs.GetFloat("X", 0),
+            PlayerPrefs.GetFloat("Y", 0),
+            PlayerPrefs.GetFloat("Z", 0)
+        );
+
+        saveButton.onClick.AddListener(Save);
+        continueButton.onClick.AddListener(Continue);
+        newGameButton.onClick.AddListener(NewGame);
+    }
+
+    void Save()
+    {
+        PlayerPrefs.SetFloat("X", transform.position.x);
+        PlayerPrefs.SetFloat("Y", transform.position.y);
+        PlayerPrefs.SetFloat("Z", transform.position.z);
+        PlayerPrefs.Save();
+        // Geen scène-wissel, blijf in huidige scène
+    }
+
+    void Continue()
+    {
+        // Laad "GameScene" als je naar een andere scène wilt
+        SceneManager.LoadScene("GAME");
+    }
+
+    void NewGame()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene("GAME"); // Start nieuwe game in "GameScene"
     }
 }
